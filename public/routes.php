05b69944a580cb3b99adbdc7aa0bc127c466e1c1
Wesley Alves <weealves1@gmail.com>
2025-05-01 19:25:49 -0300

    feat/estudantes: unifica adaptador driver

--- public/routes.php
+++ public/routes.php
@@ -1,8 +1,9 @@
 <?php
 
 use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+use Src\Adapters\Driver\API\Students\StudentAdapter;
 
+use Psr\Http\Message\ServerRequestInterface as Request;
 use Src\Adapters\Driver\API\Students\CreateStudentAdapter;
 use Src\Adapters\Driver\API\Students\SearchStudentAdapter;
 
@@ -14,8 +15,8 @@ $app->get('/', function (Request $request, Response $response, $args) {
         ->withHeader('Content-Type', 'application/json');
 });
 
-$app->get('/students/search[/{name}]', SearchStudentAdapter::class . ':execute' );
-$app->post('/students/create', CreateStudentAdapter::class . ':execute' );
+$app->get('/students/search[/{name}]', StudentAdapter::class . ':searchStudentsByName' );
+$app->post('/students/create', StudentAdapter::class . ':createStudent' );
 
 
 ?>
---
+++ src/Adapters/Driver/API/Students/StudentAdapter.php
@@ -0,0 +1,96 @@
+<?php
+namespace Src\Adapters\Driver\API\Students;
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+use Src\Core\Students\Application\InputPorts\CreateStudentServicePort;
+use Src\Core\Students\Application\InputPorts\SearchStudentServicePort;
+
+class StudentAdapter {
+    public function __construct(
+        private readonly CreateStudentServicePort $createService,
+        private readonly SearchStudentServicePort $searchService,
+    ) {}
+
+    /**
+     * Cria um novo estudante
+     *
+     * @param Request $request
+     * @param Response $response
+     * @return Response
+     */
+    public function createStudent( Request $request, Response $response ): Response{
+        $statusCode = 500;
+        $message = 'Falha ao criar estudante.';
+        $responseData = array();
+
+        $body = $request->getParsedBody();
+
+        $dto = new CreateStudentDTO(
+            $body['name'] ?? '',
+            $body['phone'] ?? '',
+            $body['email'] ?? '',
+            $body['courseId'] ?? '',
+        );
+
+        $student = $this->service->execute( $dto );
+
+        if( $student ){
+            $message = 'Estudante criado com sucesso!';
+            $statusCode = 200;
+
+            $responseData = array(
+                'id' => $student->getId(),
+                'registroAcademico' => $student->getAcademicRegistry(),
+                'nome' => $student->getName(),
+                'criadoEm' => $student->getCreatedAt(),
+            );
+        }
+
+        return $response->withJson([
+            'code' => $statusCode,
+            'mensagem' => $message,
+            'data' => $responseData,
+        ])
+        ->withStatus($statusCode)
+        ->withHeader('Content-Type', 'application/json');
+    }
+
+    /**
+     * Busca estudantes por nome;
+     *
+     * @return StudentEntity[]
+     */
+    public function searchStudentsByName( Request $request, Response $response, $args = [] ): Response{
+        $statusCode = 400;
+        $message = 'Nenhum estudante encontrado';
+        $responseData = array();
+
+        $students = $this->searchService->byName( $args['name'] ?? '' );
+
+        if( count( $students ) > 0 ){
+            $message = count( $students ).' estudante(s) encontrado(s)';
+            $statusCode = 200;
+
+            foreach ($students as $key => $student) {
+                $responseData[] = array(
+                    'nome' => $student->getName(),
+                    'criadoEm' => $student->getCreatedAt(),
+                    'registroAcademico' => $student->getAcademicRegistry(),
+                    'id' => $student->getId(),
+                );
+            }
+        }
+
+
+        return $response->withJson([
+            'code' => $statusCode,
+            'mensagem' => $message,
+            'data' => $responseData,
+        ])
+        ->withStatus($statusCode)
+        ->withHeader('Content-Type', 'application/json');
+    }
+}
+?>
